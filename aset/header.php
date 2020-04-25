60226ea1b081191764d20646238178ac33bced70
Dhikawahyu <63907574+Dhikawahyu@users.noreply.github.com>
2020-04-25 17:46:05 +0700

    Add files via upload

---
+++ aset/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html iang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, instial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+	<link rel="stylesheet" href="http://localhost/korona/aset/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="http://localhost/korona/aset/fontawesome/css/all.min.css">
+
+	<title>Korona</title>
+</head>
+<body>
+
+  	  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <a class="navbar-brand" href="#">Covid-19</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link" href="http://localhost/korona/home.php">Dashboard</a>
+      <a class="nav-item nav-link" href="http://localhost/korona/ODP/index.php">ODP</a>
+      <a class="nav-item nav-link" href="http://localhost/korona/gejala/index.php">Gejala</a>
+      <a class="nav-item nav-link" href="http://localhost/korona/logout.php">Logout</a>
+    </div>
+  </div>
+</nav>
+
+</body>
+</html>
